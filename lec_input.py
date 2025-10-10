cd4db82864ffbf1a2f9873226084f4e8ae974186
Keoflit <dsbrazhnikova@gmail.com>
2025-10-10 16:42:21 +0000

    лекция два ужас

---
+++ lec_input.py
@@ -0,0 +1,10 @@
+a = input()
+print(a)
+
+a = input("введите значение а: ")
+print(a)
+print(type(a))
+
+a = int (input("введите целое число а: "))
+print(a)
+print(type(a))
---
+++ lec_obj_ref.py
@@ -0,0 +1,6 @@
+s = 3 + 4 
+
+memory_id = id(s)
+print(s, memory_id)
+
+s = 5
