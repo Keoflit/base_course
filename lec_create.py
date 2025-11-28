a2147d595d3ba6ed5ce871b166df9731fdab383e
Keoflit <dsbrazhnikova@gmail.com>
2025-11-28 16:44:58 +0000

    ghf

---
+++ lec_create.py
@@ -0,0 +1,19 @@
+def mult_func(a):
+    x = 3* a
+    return x
+
+
+def my_print(a):
+    print(f'это мемеме и {a}')
+
+
+tmp = mult_func(4)
+print(tmp)
+
+print(mult_func(11))
+
+print(mult_func('good'))
+
+my_print('hello')
+
+my_print(mult_func('50'))
---
+++ lec_data_types.py
@@ -0,0 +1,29 @@
+x = 3
+y = 4
+
+z =  complex(x, y)
+print(z)
+
+w = complex(y, x)
+print(z + w)
+
+
+s = 'hello'
+print(s[0])
+
+#s[0] = 'H'
+
+
+t = (1, 4, 9) #это картеж 
+print(t)
+print(t[0])
+#t[0] = 3 
+
+
+d = {'key_l':4, 2: 'red', 'str':'hello'} #это дикт ключ дикшенари
+print(d['key_l'])
+print(d[2])
+print(d['str'])
+
+d['str'] = 'good' 
+print(d)
