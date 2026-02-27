bca9b9acfe28c2256c83829a25f349a8bf330286
Keoflit <dsbrazhnikova@gmail.com>
2026-02-27 16:30:27 +0000

    hghg

---
+++ lec_fhfhfh.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots (subplot_kw={"projection": "3d"})
+
+t = np.arange(0.01, 4*np.pi, 0.01)
+R =1
+x = R * np.cos(t)
+y = R * np.sin(t)
+z = R * t
+
+ax.plot(x,y,z, label="Dich")
+
+ax.legend()
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+ax.set_title('3D Test')
